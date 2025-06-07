203d7b57a71051c5b8739e574be7f6b5b2da6af5
Maisan Auliya <maisanaulia02@gmail.com>
2025-06-07 20:36:07 +0700

    updating README.md

--- lib/src/core/enums/classification_model_type.dart
+++ lib/src/core/enums/classification_model_type.dart
@@ -16,7 +16,7 @@ class ClassificationModelTypeEnums {
       case ClassificationModelType.auliya:
         return '$_baseModels/auliya_model.tflite';
       case ClassificationModelType.fuadah:
-        return '$_baseModels/model_ikan_float32.tflite';
+        return '$_baseModels/fuadah_model.tflite';
     }
   }
 
